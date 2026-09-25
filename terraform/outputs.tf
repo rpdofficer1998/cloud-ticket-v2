0740ad1931631022770386045f6bd5d63ca1e577
tanzy211 <neotanzy211@outlook.com>
2026-09-25 13:08:34 +1200

    Expose deployment resource outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -17,3 +17,23 @@ output "database_subnet_ids" {
   description = "CloudTicket V2 private database subnet IDs"
   value       = module.networking.database_subnet_ids
 }
+
+output "ec2_instance_id" {
+  description = "ID of the CloudTicket application EC2 instance"
+  value       = module.ec2.instance_id
+}
+
+output "ec2_private_ip" {
+  description = "Private IP address of the CloudTicket application EC2 instance"
+  value       = module.ec2.private_ip
+}
+
+output "ecr_repository_name" {
+  description = "Name of the CloudTicket backend ECR repository"
+  value       = module.ecr.repository_name
+}
+
+output "ecr_repository_url" {
+  description = "URL of the CloudTicket backend ECR repository"
+  value       = module.ecr.repository_url
+}
